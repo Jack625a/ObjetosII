ca4a843372c95a22798dd42985a6624c67d158b3
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-07-25 12:11:51 -0400

    Tipos de datos

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,1 +1,15 @@
 ﻿Console.WriteLine("Primer Codigo");
+//Tipos de Datos 
+//1. Tipos de datos Numericos
+int numero=12; //Enteros
+double numero2=3.14161922; // Decimales de mayor alcance
+float numero3=4.5; //Decimales de menor alcane
+//2. Tipos de Datos Cadenas de Caracteres
+char materia="Objetos II"; //Cadena de caracter
+//3. Tipos de datos Logicos - Booleanos
+bool encender=true;
+//Tipos de datos Compuestos
+//1. Fecha y Hora
+DateTime fechaactual=DateTime.Now;
+//2. Tipos Array []
+int[] edades={24,12,25,36,45,75,15};
