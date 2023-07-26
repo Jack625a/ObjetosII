a20e9dcf928b697723d6f446b36d4cec7d67b584
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-07-26 11:25:41 -0400

    Operadores en C#

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -3,9 +3,10 @@
 //1. Tipos de datos Numericos
 int numero=12; //Enteros
 double numero2=3.14161922; // Decimales de mayor alcance
-float numero3=4.5; //Decimales de menor alcane
+float numero3=4.5F; //Decimales de menor alcane
 //2. Tipos de Datos Cadenas de Caracteres
-char materia="Objetos II"; //Cadena de caracter
+string materia="Objetos II"; //Cadena de caracter
+char letra='A';
 //3. Tipos de datos Logicos - Booleanos
 bool encender=true;
 //Tipos de datos Compuestos
@@ -13,3 +14,40 @@ bool encender=true;
 DateTime fechaactual=DateTime.Now;
 //2. Tipos Array []
 int[] edades={24,12,25,36,45,75,15};
+
+//Imprension en Pantalla
+Console.WriteLine(numero);
+Console.WriteLine(fechaactual);
+Console.WriteLine(numero2);
+Console.WriteLine(numero3);
+Console.WriteLine(materia);
+Console.WriteLine(encender);
+
+//Operadores en C#
+//Operadores Aritmeticos
+//Suma, Resta, Multiplicacion, Division, Modulo
+int num1=45;
+int num2=30;
+int suma=num1+num2;
+int resta=num1-num2;
+int multiplicacion=num1*num2;
+int division=num1/num2;
+Console.WriteLine("SUMA");
+Console.WriteLine(suma);
+Console.WriteLine("RESTA");
+Console.WriteLine(resta);
+Console.WriteLine("MULTIPLICACION");
+Console.WriteLine(multiplicacion);
+Console.WriteLine("DIVISION");
+Console.WriteLine(division);
+Console.WriteLine("MODULO");
+Console.WriteLine(num1%2);
+//Operadores de cadenas de caracteres
+//Concatenar
+//Operadores Logicos
+//Mayor que, Menor que, Igual que, Distinto que, y, o
+Console.WriteLine(num1>=num2); //True
+Console.WriteLine(num1<=num2); //False
+Console.WriteLine(num1==num2); //False
+Console.WriteLine(num1!=num2); //True
+
