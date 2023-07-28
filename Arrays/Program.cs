4c81a9fffe8e7a74286e7d091cfc11147b3a487f
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-07-28 12:09:09 -0400

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿//Ejercicio 1. Encontrar el numero mayor de un array
+using System;
+class Program //Paso 1 Definir la clase
+{
+    //Paso dos definir los atributos o parametros
+    static void Main()
+    {   
+        //Paso 2
+        Console.WriteLine("Ingrese la cantidad de numeros: ");
+        int cantidad=Convert.ToInt32(Console.ReadLine());
+
+        int[] numeros=new int[cantidad];
+        for (int i=0;i<cantidad;i++)
+        {
+            Console.WriteLine("Ingrese el numero :");
+            numeros[i]=Convert.ToInt32(Console.ReadLine());
+        }
+        //Paso 3
+        int mayor=numeros[0];
+        for (int i=1;i<cantidad;i++)
+        {
+            if(numeros[i]>mayor)
+            {
+                mayor=numeros[i];
+            }
+        }
+    Console.WriteLine("El numero mayor es: "+mayor);
+    }
+}
